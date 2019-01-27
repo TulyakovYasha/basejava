29260bb8f4915ef1ba795b432df65c371fb5c854
TulyakovYasha <tulyakov.yasha@gmail.com>
2019-01-27 18:56:07 +0200

    Upgrade methods save, get, delete, size, clear, getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,73 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int i = 0;
+        while (true) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            } else {
+                i++;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = 0;
+        Resume resume = new Resume();
+        while (storage[i] != null) {
+            if (storage[i].toString().equals(uuid)) {
+                resume = storage[i];
+                return resume;
+            } else i++;
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while (true) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+                break;
+            } else {
+                i++;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                size++;
+            }
+        }
+        Resume[] resumes = new Resume[size];
+        int number = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                resumes[number] = storage[i];
+                number++;
+            }
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
