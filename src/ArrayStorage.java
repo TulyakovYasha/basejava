7fccdd928f20beeec56ef641b00f410fae0bf4eb
TulyakovYasha <tulyakov.yasha@gmail.com>
2019-01-28 00:01:09 +0200

    Upgrade methods "getAll", "delete", "size", "get"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -24,27 +24,28 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        int i = 0;
-        Resume resume = new Resume();
-        while (storage[i] != null) {
-            if (storage[i].toString().equals(uuid)) {
-                resume = storage[i];
-                return resume;
-            } else i++;
+        while (true) {
+            for (Resume resume : storage) {
+                if (resume.toString().equals(uuid)) {
+                    return resume;
+                } else {
+                    return null;
+                }
+            }
         }
-        return resume;
     }
 
     void delete(String uuid) {
-        int i = 0;
-        while (true) {
-            if (storage[i].toString().equals(uuid)) {
+        Resume[] resumes = new Resume[storage.length - 1];
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
-                break;
-            } else {
-                i++;
             }
         }
+        for (int i = 1; i < resumes.length; i++) {
+            resumes[i - 1] = storage[i];
+        }
+        storage = resumes;
     }
 
     /**
@@ -58,17 +59,19 @@ public class ArrayStorage {
             }
         }
         Resume[] resumes = new Resume[size];
-        int number = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                resumes[number] = storage[i];
-                number++;
-            }
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
         }
         return resumes;
     }
 
     int size() {
-        return storage.length;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
