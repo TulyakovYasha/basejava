f12c87915e234c2f5679a2baeafd253a5f224848
TulyakovYasha <tulyakov.yasha@gmail.com>
2019-02-11 20:05:38 +0200

    add Update

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,3 @@
-
-
 /**
  * Array based storage for Resumes
  */
@@ -8,41 +6,75 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int sizeWithoutNull = 0;
 
-    void clear() {
+    public boolean exsists(String uuid){
+        for (int i =0; i < sizeWithoutNull; i++){
+            if(storage[i].uuid.equals(uuid)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clear() {
         for (int i = 0; i < sizeWithoutNull; i++) {
             storage[i] = null;
         }
         sizeWithoutNull = 0;
     }
 
-    void save(Resume r) {
-        storage[sizeWithoutNull] = r;
-        sizeWithoutNull++;
+    public void update(Resume resume){
+        if(exsists(resume.uuid)){
+            for (int i =0; i< sizeWithoutNull; i++){
+                if(resume.uuid.equals(storage[i].uuid)){
+                    storage[i] = resume;
+                }
+            }
+        }else{
+            System.out.println("No resume in update");
+        }
     }
 
-    Resume get(String uuid) {
-        for (int i = 0; i < sizeWithoutNull; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
+    public void save(Resume r) {
+        if(sizeWithoutNull < storage.length){
+            if(!exsists(r.uuid)){
+                storage[sizeWithoutNull] = r;
+                sizeWithoutNull++;
             }
+        }else {
+            System.out.println("Will be exception");
+        }
+    }
+
+    public Resume get(String uuid) {
+        if(exsists(uuid)) {
+            for (int i = 0; i < sizeWithoutNull; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        }else {
+            System.out.println("No resume in get");
         }
         return null;
     }
 
-    void delete(String uuid) {
-        for (int i = 0; i < sizeWithoutNull; i++) {
-            if(storage[i].uuid.equals(uuid)){
-                sizeWithoutNull--;
-                System.arraycopy(storage, i+1, storage, i, sizeWithoutNull - i);
+    public void delete(String uuid) {
+        if(exsists(uuid)) {
+            for (int i = 0; i < sizeWithoutNull; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    sizeWithoutNull--;
+                    System.arraycopy(storage, i + 1, storage, i, sizeWithoutNull - i);
+                }
             }
+        }else {
+            System.out.println("No resume in delete");
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] resumes = new Resume[sizeWithoutNull];
         for (int i = 0; i < sizeWithoutNull; i++) {
             resumes[i] = storage[i];
@@ -50,7 +82,7 @@ public class ArrayStorage {
         return resumes;
     }
 
-    int size() {
+    public int size() {
         return sizeWithoutNull;
     }
 }
