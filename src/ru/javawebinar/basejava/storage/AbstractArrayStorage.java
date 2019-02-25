9d058973c2ec995dd37f15a9a27a8670ea4ce482
TulyakovYasha <tulyakov.yasha@gmail.com>
2019-02-25 19:51:22 +0200

    HW 4 Add Tests Add method toString(); + Exception

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
